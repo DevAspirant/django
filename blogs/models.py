9d037f7388b924e907e895c65abf0d021f66f8c4
devAspirant <devAspirant@gmail.com>
2019-02-24 11:48:41 +0300

    3rd

--- blogs/models.py
+++ blogs/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class blogs(models.Model):
+class SignUp(models.Model):
     email = models.EmailField()
     full_name = models.CharField(max_length=15,blank=True,null=True)
     timestamp = models.DateTimeField(auto_now_add=True,auto_now=False)
