ae02e6f3320103042052e47620d26a6a6fed81bd
devAspirant <devAspirant@gmail.com>
2019-02-24 11:40:39 +0300

    second commit

--- blogs/models.py
+++ blogs/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class blogs(models.Model):
     email = models.EmailField()
-    full_name = models.CharField(max_length=15)
-    timestamp = models.DateTimeField()
-    updated = models.DateTimeField()
+    full_name = models.CharField(max_length=15,blank=True,null=True)
+    timestamp = models.DateTimeField(auto_now_add=True,auto_now=False)
+    updated = models.DateTimeField(auto_now_add=True,auto_now=False)
 
     def __unicode__(self):
         return self
